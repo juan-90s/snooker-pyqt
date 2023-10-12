c4e2a6e7c68b9dc89a74d4ec96398a54372ad204
Juan Woo <juanowwo@gmail.com>
2023-10-11 23:16:52 -0400

    - minor optimize on physics

--- physics/core.py
+++ physics/core.py
@@ -17,19 +17,25 @@ class PhysicsManager():
         if timer:
             self._timer = timer
         else:
-            self._timer = Timer(self)
-            self._timer.start(1000 // tps)
+            self._timer = Timer()
+            self._timer.start(1000 // self.tps)
         self._timer.timeout.connect(self.update)
         self.dt = self._timer.interval() / 1000
         
         self._bodies = []
+        self._statics = []
         self._mutex = Mutex()
 
         self.frametime = 0
         self.global_friction = 0
+        self.gravity = 0
+        self.gravity_center = None
     
     def add_body(self, body: Body):
-        self._bodies.append(body)
+        if body.type == Body.Type.Static:
+            self._statics.append(body)
+        else:
+            self._bodies.append(body)
 
     def add_bodies(self, bodies: list[Body]):
         self._bodies = self._bodies + bodies
@@ -42,9 +48,9 @@ class PhysicsManager():
     def solve_movement(self, dt):
         # movement
         for body in self._bodies:
-            if body.type == Body.Type.Static:
-                continue
             body.update(dt)
+            if body.type is not Body.Type.Dynamic:
+                continue
             if self.global_friction > 0 and body.linear_velocity.lengthSquared() > 0:
                 # apply slow donw effect
                 linear_velocity_after = body.linear_velocity - (1 - self.global_friction * body.friction) * dt * body.linear_velocity - self.global_friction * dt * body.linear_velocity.normalized()
@@ -57,13 +63,26 @@ class PhysicsManager():
     def solve_contact(self):
         # contact
         for bodyA in self._bodies:
-            if bodyA.type == Body.Type.Static:
-                continue
 
+            if bodyA.linear_velocity.lengthSquared() <= 0.001:
+                continue
             for bodyB in self._bodies:
+                if (bodyA.position - bodyB.position).lengthSquared() > (bodyA.shape.radius+bodyB.shape.radius)**2:
+                    continue
                 contact = Contact(bodyA, bodyB)
                 if(contact.intersect):
-                    contact.resolve()       
+                    contact.resolve()  
+
+            for bodyS in self._statics:
+
+                contact = Contact(bodyA, bodyS)
+                if(contact.intersect):
+                    vel_normal = Vec2.dotProduct(bodyA.linear_velocity, contact.normal)
+                    contact.resolve()
+                    if vel_normal < 0.01:
+                        bodyA.linear_velocity -= vel_normal * contact.normal
+
+ 
 
     @Slot()
     def update(self):
@@ -74,6 +93,8 @@ class PhysicsManager():
         self.frametime = 1000 * (timeit.default_timer() - t)
         self._mutex.unlock()
 
+
+# Do not use
 class PhysicsManager_Grid(PhysicsManager):
     def __init__(self, timer:Timer, world_width, world_height, grid_width, grid_height):
         super().__init__(timer)
